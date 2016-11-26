1b3c13fc0f74974d8eeb26c42c72ae525aff9b9f
Winbobob <zhu6@ncsu.edu>
2016-11-26 12:18:40 -0500

    Finish test_separator.js

--- checker/test_separator.js
+++ checker/test_separator.js
@@ -1,45 +1,64 @@
 var readline = require('linebyline');
 var fs = require('fs');
+var S = require('string');
 
 function main()
 {
 	var args = process.argv.slice(2);
 	if( args.length == 0 )
 	{
+		// default test file
 		args = [__dirname + '/../../solar-calc/test/test.js'];
 	}
 	var filePath = args[0];
-	
 	TestSeparator(filePath);
 }
 
 function TestSeparator(filePath)
 {
 	var testCaseIterator = 0;
+	// code block before test case
+	var lineIterator = 0;
+	var filePreBlock = '';
+	var testPreBlock = '';
+	var testBlock = '';
+
 	file = readline(filePath);
 	file.on('line', function(line, lineCount, byteCount) {
-		// code block before test case
-		lineIterator = 0;
-		filePreBlock = '';
-		testPreBlock = '';
-		testBlock = '';
+		// each test case has 3 line
+		// first 2 line has different content
+		// last line is just curly brace
 		if (lineIterator < 2) {
-			
 			if (lineCount < 8) {
+				// if line number < 8, directly adding into fileProBlock
 				filePreBlock += (line + '\n');
-			} else if (line.indexOf('describe')) {
+			} else if (line.indexOf('describe') > 0) {
+				// if line number > 8, and this line including 'describe'
+				// empty the variable and add it into testPreBlock 
 				testPreBlock = ''
 				testPreBlock += (line + '\n');
-			} else if (!line.indexOf('it') && !line.indexOf('assert')){
-				testPreBlock += (line + '\n');
+			} else if (line.indexOf('it') == -1 && line.indexOf('assert') == -1){
+				// if line number > 8, and this line not including 'describe'/'it'/'assert'
+				// if this line is not empty, add it into test PreBlock
+				if(!S(line).isEmpty()) { testPreBlock += (line + '\n'); }
 			} else {
+				// if line number > 8, and this line not including 'describe'
+				// but this line include 'it' or'assert'
+				// if this line is not empty, add it into test testBlock
 				lineIterator += 1;
-				testBlock += (line + '\n');
+				if(!S(line).isEmpty()) { testBlock += (line + '\n'); }
 			}
 		} else {
 			testCaseIterator += 1;
+			// concatenate all blocks
 			var fileContent = filePreBlock + testPreBlock + testBlock;
-			var filePath = __dirname + '/../../solar-calc/separated_tests/test_' + testCaseIterator;
+			// add ending curly braces
+			fileContent += '    });\n'
+			fileContent += '  });\n'
+			fileContent += '});\n'
+
+			// write content into file
+			var filePath = __dirname + '/../../solar-calc/separated_tests/test_' + testCaseIterator + '.rb';
 			fs.writeFile(filePath, fileContent, function(err) {
 				if (err) { throw err }
 			});
@@ -48,8 +67,6 @@ function TestSeparator(filePath)
 			lineIterator = 0;
 			testBlock = '';
 		}
-		
-
 	}).on('error', function(e) {
 		throw e;
 	});
