2cf477a05d92f093b10fd74327dd515c3fe91745
Winbobob <zhu6@ncsu.edu>
2016-11-26 23:00:16 -0500

    Rename checker/test_covered_method_finder.js -> checker/test_covered_method_finder_legacy.js

--- checker/test_covered_method_finder_legacy.js
+++ checker/test_covered_method_finder_legacy.js
@@ -1,23 +1,27 @@
 var readline = require('linebyline');
 var exec = require('child_process').exec;
-var fs = require('fs-extra');
+var fs = require('fs');
 var S = require('string');
 
 var appDir = __dirname + '/../../solar-calc/';
 var sourceFileArr = ['moon.js', 'solarCalc.js', 'sun.js'];
+var coverageFilePath = appDir + 'coverage/coverage.json';
+var coverageFinalFilePath = appDir + 'coverage/coverage-final.json';
 
 function main(){
 	var separatedTestFolder = appDir + 'separated_tests/';
 	fs.readdir(separatedTestFolder, function(err, files){
 		files.forEach(function(testFileName){
+			// if(fs.existsSync(coverageFilePath)) { fs.unlinkSync(coverageFilePath); }
+			// if(fs.existsSync(coverageFinalFilePath)) { fs.unlinkSync(coverageFinalFilePath); }
 			// ./node_modules/.bin/istanbul cover --report json ./node_modules/.bin/_mocha separated_tests/test_3.rb
 			var command = './node_modules/.bin/istanbul cover --report json ./node_modules/.bin/_mocha separated_tests/' + testFileName;
-			var child = exec(command, {cwd: appDir}, function(error, stdout, stderr){
-				if(error) { throw error; }
-				var jsonFilePath = appDir + 'coverage/coverage-final.json';
-				var jsonContent = JSON.parse(fs.readFileSync(jsonFilePath, 'utf8'));
-				parseIstanbulOutput(testFileName, jsonContent);
-			});
+			exec(command, {cwd: appDir});
+			var jsonContent = JSON.parse(fs.readFileSync(coverageFinalFilePath, 'utf8'));
+		// console.log('==' + testFileName + '======');
+		// console.log(jsonContent);
+		// console.log('========');
+			parseIstanbulOutput(testFileName, jsonContent);
 		});
 	});
 }
@@ -28,12 +32,14 @@ function parseIstanbulOutput(testFileName, jsonContent){
 		// function coverage info
 		var key = S(__dirname).replaceAll('DevOps-M4/checker', 'solar-calc/lib/') + sourceFile;
 		var funcCoverage = jsonContent[key]['f'];
-
+// console.log(testFileName);
+// console.log(funcCoverage);
 		var coveredFunc = [];
 		for(var num in funcCoverage){
 			if(funcCoverage[num] > 0) { coveredFunc.push(num); }
 		}
 		console.log(testFileName + '-' + sourceFile + ': ' + coveredFunc);
+		console.log('========');
 	});
 }
 
