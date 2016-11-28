16332dfd899e025ad0353ce804ebe257f7461a13
Winbobob <zhu6@ncsu.edu>
2016-11-28 17:32:30 -0500

    Update

--- checker/get_testcase_id.js
+++ checker/get_testcase_id.js
@@ -1,21 +1,55 @@
 var project = __dirname + '/../../solar-calc'
 var fs = require("fs");
 var readline = require('linebyline');
+var testFiles = [];
 
-file = readline(project + '/test/test.js');
-file.on('line', function(line, lineCount, byteCount) {
-	start = line.indexOf('it(');
+function getTestFiles(callback){
+	var file = readline(__dirname + '/../results/commit_touched_testcases');
+	file.on('line', function(line, lineCount, byteCount) {
+		if(lineCount == 1) 
+		{ 
+			testFiles = line.split(','); 
+			callback(testFiles);
+		}
+	}).on('error', function(e) {
+		throw e;
+	});
+}
 
-	if(start >= 0){
-		start += 4
-		end = line.substring(start).indexOf('\'');
-		testCaseDesc = line.substring(start, start + end);
-		console.log(testCaseDesc);
-		// write test case description into file
-		fs.appendFile(__dirname + '/../results/test_case_desc', testCaseDesc + '\n', function (err) {
-		  if (err) { throw err; }
+getTestFiles(function(testFiles){
+	testFiles.forEach(function(testFileName){
+		file = readline(project + '/separated_tests/' + testFileName);
+		file.on('line', function(line, lineCount, byteCount) {
+			start = line.indexOf('it(');
+
+			testCaseDesc = ''
+			if(start >= 0){
+				start += 4
+				end = line.substring(start).indexOf('\'');
+				testCaseDesc += line.substring(start, start + end) + '|';
+				// console.log(testCaseDesc);
+				// write test case description into file
+
+				
+			}
+
+			testCaseDesc = testCaseDesc.substring(0, testCaseDesc.lastIndexOf('|'))
+			console.log(testCaseDesc)
+			fs.writeFile(__dirname + '/../results/commit_touched_testcases_desc', testCaseDesc, function (err) {
+			  if (err) { throw err; }
+			});
+		}).on('error', function(e) {
+			throw e;
 		});
-	}
-}).on('error', function(e) {
-	throw e;
+	});
+});	
+
+fs.readFile(__dirname + '/../results/commit_touched_testcases_desc', 'utf8', function (err,data) {
+  if (err) {
+    return console.log(err);
+  }
+
+  fs.writeFile(__dirname + '/../results/commit_touched_testcases_desc', data.substring(0, data.lastIndexOf('|')), 'utf8', function (err) {
+     if (err) return console.log(err);
+  });
 });
